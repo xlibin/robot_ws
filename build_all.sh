0d7fa7042179351aa35414120425f41f32ffb596
NightFury <1218138462@qq.com>
2018-04-27 15:27:17 +0800

    update build_all

--- build_all.sh
+++ build_all.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 catkin_make clean
-catkin_make -DCATKIN_WHITELIST_PACKAGES="people_msgs"
-catkin_make -DCATKIN_WHITELIST_PACKAGES="move_base_msgs"
-catkin_make -DCATKIN_WHITELIST_PACKAGES="awaken_asr"
+catkin_make -DCATKIN_WHITELIST_PACKAGES="people_msgs;move_base_msgs;awaken_asr" --make-args -j4
 catkin_make -DCATKIN_WHITELIST_PACKAGES="" --make-args -j4
